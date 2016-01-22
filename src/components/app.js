0158d867b7aa51ca41eab649cf82b71371fffd70
Fengming Sun <s@sfmblog.cn>
2016-01-23 05:01:37 +0800

    fix

--- src/components/app.js
+++ src/components/app.js
@@ -8,58 +8,58 @@ import {VisibilityFilters} from '../actions/action-types';
 import {addTodo, completeTodo, setVisibilityFilter} from '../actions/todos.js';
 
 class App extends Component {
-render() {
-const {dispatch, visibleTodos, visibilityFilter} = this.props;
-return (
-<div>
-<AddTodo
-onAddClick={text => {
-dispatch(addTodo(text));
-}} />
-<TodoList
-todos={visibleTodos}
-onTodoItemClick={index => {
-dispatch(completeTodo(index))
-}} />
-<TodoListSwitcher
-filter={visibilityFilter}
-onFilterChange={filter => {
-dispatch(setVisibilityFilter(filter));
-}} />
-</div>
-)
-}
+  render() {
+    const {dispatch, visibleTodos, visibilityFilter} = this.props;
+    return (
+      <div>
+        <AddTodo
+          onAddClick={text => {
+            dispatch(addTodo(text));
+          }} />
+        <TodoList
+          todos={visibleTodos}
+          onTodoItemClick={index => {
+            dispatch(completeTodo(index))
+          }} />
+        <TodoListSwitcher
+          filter={visibilityFilter}
+          onFilterChange={filter => {
+            dispatch(setVisibilityFilter(filter));
+          }} />
+      </div>
+    )
+  }
 }
 
 App.propTypes = {
-visibleTodos: ImmutablePropTypes.listOf(
-ImmutablePropTypes.contains({
-text: PropTypes.string.isRequired,
-completed: PropTypes.bool.isRequired
-})).isRequired,
-visibilityFilter: PropTypes.oneOf([
-VisibilityFilters.SHOW_ALL,
-VisibilityFilters.SHOW_COMPLETED,
-VisibilityFilters.SHOW_ACTIVE
-]).isRequired
+  visibleTodos: ImmutablePropTypes.listOf(
+    ImmutablePropTypes.contains({
+      text: PropTypes.string.isRequired,
+      completed: PropTypes.bool.isRequired
+    })).isRequired,
+    visibilityFilter: PropTypes.oneOf([
+      VisibilityFilters.SHOW_ALL,
+      VisibilityFilters.SHOW_COMPLETED,
+      VisibilityFilters.SHOW_ACTIVE
+    ]).isRequired
 };
 
 function selectTodos(todos, filter) {
-switch(filter) {
-case VisibilityFilters.SHOW_ALL:
-return todos;
-case VisibilityFilters.SHOW_COMPLETED:
-return todos.filter(todo => todo.get('completed'));
-case VisibilityFilters.SHOW_ACTIVE:
-return todos.filter(todo => !todo.get('completed'));
-}
+  switch(filter) {
+    case VisibilityFilters.SHOW_ALL:
+      return todos;
+    case VisibilityFilters.SHOW_COMPLETED:
+      return todos.filter(todo => todo.get('completed'));
+    case VisibilityFilters.SHOW_ACTIVE:
+      return todos.filter(todo => !todo.get('completed'));
+  }
 }
 
 function select(state) {
-return {
-visibleTodos: selectTodos(state.todos, state.visibilityFilter),
-visibilityFilter: state.visibilityFilter
-};
+  return {
+    visibleTodos: selectTodos(state.todos, state.visibilityFilter),
+    visibilityFilter: state.visibilityFilter
+  };
 }
 
 export default connect(select)(App);
--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -3,29 +3,29 @@ import ImmutablePropTypes from 'react-immutable-proptypes';
 import TodoItem from './todo-item';
 
 export default class TodoList extends Component {
-render() {
-let todoItems = [];
-this.props.todos.map((todo, index) => {
-let props = todo.toObject();
-todoItems.push(
-<TodoItem 
-  {...props}
-  key={index}
-onClick={() => this.props.onTodoItemClick(index)} />
-);
-});
-return (
-<ul>
-  {todoItems}
-</ul>
-);
-}
+  render() {
+    let todoItems = [];
+    this.props.todos.map((todo, index) => {
+      let props = todo.toObject();
+      todoItems.push(
+        <TodoItem 
+          {...props}
+          key={index}
+          onClick={() => this.props.onTodoItemClick(index)} />
+      );
+    });
+    return (
+      <ul>
+        {todoItems}
+        </ul>
+    );
+  }
 }
 TodoList.propTypes = {
-onTodoItemClick: PropTypes.func.isRequired,
-todos: ImmutablePropTypes.listOf(
-ImmutablePropTypes.contains({
-text: PropTypes.string.isRequired,
-completed: PropTypes.bool.isRequired
-})).isRequired
+  onTodoItemClick: PropTypes.func.isRequired,
+  todos: ImmutablePropTypes.listOf(
+    ImmutablePropTypes.contains({
+      text: PropTypes.string.isRequired,
+      completed: PropTypes.bool.isRequired
+    })).isRequired
 };
